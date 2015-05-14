28e91f92d4fed18aead2c7a04882586a14139599
Gabi Dobocan <gabi@appscend.com>
2015-05-14 14:42:52 +0000

    Added X-BLGREQ-SIG to allowed headers list

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ db.Couchbase.bucket.on('connect', function OnBucketConnect() {
 
 	app.use(function(req, res, next) {
 	  res.header("Access-Control-Allow-Origin", "*");
-	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-BLGREQ-SIGN");
 	  if ('OPTIONS' == req.method) {
 	      res.send(200);
 	    }
