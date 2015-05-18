75274a31bb580ae28d56c308cd1603496b434805
Razvan Botea <razvan@appscend.com>
2015-05-18 14:17:12 +0300

    cleaned up code x2

--- controllers/device.js
+++ controllers/device.js
@@ -3,30 +3,29 @@ var router = express.Router();
 var Models = require('octopus-models-api');
 var uuid = require('uuid');
 var security = require('./security');
-var Models = require('octopus-models-api');
 
 router.use(security.keyValidation);
 
 router.post('/register', function(req, res, next) {
-  if (req.get('X-BLGREQ-UDID') == '') {
-    req.body.id = uuid.v4();
+	if (req.get('X-BLGREQ-UDID') == '') {
+		req.body.id = uuid.v4();
 
-    Models.Subscription.addDevice(req.body, function(err, result) {
-      if (!err) {
-        return res.status(200).json({status: 200, identifier: req.body.id}).end();
-      }
+		Models.Subscription.addDevice(req.body, function(err, result) {
+			if (!err) {
+				return res.status(200).json({status: 200, identifier: req.body.id}).end();
+			}
 
-      next(err);
-    });
-  } else {
-    req.body.id = req.get('X-BLGREQ-UDID');
+			next(err);
+		});
+	} else {
+		req.body.id = req.get('X-BLGREQ-UDID');
 
-    Models.Subscription.updateDevice(req.body, function(err, result) {
-      if (err) return next(err);
+		Models.Subscription.updateDevice(req.body, function(err, result) {
+			if (err) return next(err);
 
-      res.status(200).json({status:200, message: "Device has been updated"});
-    });
-  }
+			res.status(200).json({status:200, message: "Device has been updated"});
+		});
+	}
 });
 
 module.exports = router;
