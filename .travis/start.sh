d59a4875fce769e63512a175b8d4debf92e9a607
Andrei Marinescu <andrei@appscend.com>
2015-09-19 15:45:24 -0400

    Pull telepat-worker repo in travis tests

--- .travis/start.sh
+++ .travis/start.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+pwd
+git clone -b develop https://github.com/telepat-io/telepat-worker.git
+cd telepat-worker
+npm install
+cd ..
 forever start --append --uid "aggregation" telepat-worker/index.js -t aggregation -i 0
 forever start --append --uid "write" telepat-worker/index.js -t write -i 0
 forever start --append --uid "update_friends" telepat-worker/index.js -t update_friends -i 0
