c4d76249e43f7cf1bf6d0f82d0f00af93c106a81
Andrei Marinescu <andrei@appscend.com>
2015-09-19 14:51:11 -0400

    Increased wait time

--- test/common.js
+++ test/common.js
@@ -13,4 +13,4 @@ exports.crypto = crypto;
 exports.url = 'http://localhost:3000';
 exports.appKey = appKey;
 exports.appIDsha256 = crypto.SHA256(appKey).toString(crypto.enc.Hex);
-exports.DELAY = 400;
+exports.DELAY = 500;
