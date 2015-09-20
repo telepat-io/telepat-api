9ce5c11286f62787371cc7577d59e81556155ec7
Gabi Dobocan <gabi@telepat.io>
2015-09-20 18:17:16 -0400

    add unit test logging

--- test/common.js
+++ test/common.js
@@ -4,15 +4,39 @@ var request = require('supertest');
 var crypto = require('crypto-js');
 
 var appKey = "3406870085495689e34d878f09faf52c";
-
-exports.should = should;
-exports.assert = assert;
-exports.request = request;
-exports.crypto = crypto;
-
+var logLevel = process.env.TP_TST_LOG || 1;
 exports.url = 'http://localhost:3000';
 exports.appKey = appKey;
 exports.appIDsha256 = crypto.SHA256(appKey).toString(crypto.enc.Hex);
 exports.DELAY = 400;
+exports.logLevel = logLevel;
 
-// exports.call = function (endpoint, appID, authValue, UDID, method)
+function highjackEnd(request) {
+  var end = request.end;
+  request.end = function (callback) {
+    end.call(this, function (err, res) {
+      if (logLevel === 2 || (err && logLevel === 1)) {
+        console.log(res.body);
+      }
+      callback(err, res);
+    })
+  }
+  return request;
+}
+
+exports.should = should;
+exports.assert = assert;
+exports.crypto = crypto;
+
+exports.request = function (url) {
+  var rq = request(url);
+  var get = rq.get;
+  rq.get = function(path) {
+    return highjackEnd(get.call(this, path))
+  }
+  var post = rq.post;
+  rq.post = function(path) {
+    return highjackEnd(post.call(this, path))
+  }
+  return rq;
+}
