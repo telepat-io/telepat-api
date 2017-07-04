f03142f3f16c48f6284a59ba576702adbf90b4bc
Adelina Enache <adetomi27@gmail.com>
2017-07-04 13:46:08 +0300

    chaged start script to use telepat models from develop branch

--- .travis/start.sh
+++ .travis/start.sh
@@ -3,7 +3,11 @@ pwd
 git clone -b develop https://github.com/telepat-io/telepat-worker.git
 cd telepat-worker
 npm install
+rm -rf node_modules/telepat-models
+npm install https://github.com/telepat-io/telepat-models.git#develop
 cd ..
+rm -rf node_modules/telepat-models
+npm install https://github.com/telepat-io/telepat-models.git#develop
 forever start --append --uid "aggregation" -o telepat-worker/aggregation.out -e telepat-worker/aggregation.err telepat-worker/index.js -t aggregation -i 0
 forever start --append --uid "write" -o telepat-worker/write.out -e telepat-worker/write.err telepat-worker/index.js -t write -i 0
 forever start --append --uid "transport_manager" -o telepat-worker/update_friends.out -e telepat-worker/update_friends.err telepat-worker/index.js -t update_friends -i 0
