f34a2b7a54d307dce997f81273772d658f47eeb5
Adelina Enache <adetomi27@gmail.com>
2017-07-05 13:58:36 +0300

    start script modifies config file

--- .travis/start.sh
+++ .travis/start.sh
@@ -8,6 +8,15 @@ npm install https://github.com/telepat-io/telepat-models.git#develop
 cd ..
 rm -rf node_modules/telepat-models
 npm install https://github.com/telepat-io/telepat-models.git#develop
+touch config.json
+cat config.example.json > config.json
+sed -i 's/\"message_queue\": \"kafka\"/\"message_queue\": \"amqp\"/' config.json
+sed -i 's/hostname/localhost/' config.json
+sed -i 's/10\.0\.0\../localhost/' config.json
+sed -i 's/telepat/guest/' config.json
+sed -i 's/\"password\": \"password\"/\"password\": \"guest\"/' config.json
+cp config.json telepat-worker/config.json
+cat config.json
 forever start --append --uid "aggregation" -o telepat-worker/aggregation.out -e telepat-worker/aggregation.err telepat-worker/index.js -t aggregation -i 0
 forever start --append --uid "write" -o telepat-worker/write.out -e telepat-worker/write.err telepat-worker/index.js -t write -i 0
 forever start --append --uid "transport_manager" -o telepat-worker/update_friends.out -e telepat-worker/update_friends.err telepat-worker/index.js -t update_friends -i 0
