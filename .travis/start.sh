fe7c1f4b016c56268e96867917a8bfa53b14be26
Adelina Enache <adetomi27@gmail.com>
2017-07-05 17:34:16 +0300

    start.sh should start transport_manager instead of update_friends

--- .travis/start.sh
+++ .travis/start.sh
@@ -19,7 +19,7 @@ cp config.json telepat-worker/config.json
 cat config.json
 forever start --append --uid "aggregation" -o telepat-worker/aggregation.out -e telepat-worker/aggregation.err telepat-worker/index.js -t aggregation -i 0
 forever start --append --uid "write" -o telepat-worker/write.out -e telepat-worker/write.err telepat-worker/index.js -t write -i 0
-forever start --append --uid "transport_manager" -o telepat-worker/update_friends.out -e telepat-worker/update_friends.err telepat-worker/index.js -t update_friends -i 0
+forever start --append --uid "transport_manager" -o telepat-worker/transport_manager.out -e telepat-worker/transport_manager.err telepat-worker/index.js -t transport_manager -i 0
 forever start --append --uid "android" -o telepat-worker/android.out -e telepat-worker/android.err telepat-worker/index.js -t android_transport -i 0
 forever start --append --uid "ios" -o telepat-worker/ios.out -e telepat-worker/ios.err telepat-worker/index.js -t ios_transport -i 0
 exit 0
